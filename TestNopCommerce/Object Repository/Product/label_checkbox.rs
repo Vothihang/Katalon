6bd95cf0aed635e72f6c2dd67851eae789245c48
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:27:11 +0700

    Create label_checkbox.rs

---
+++ TestNopCommerce/Object Repository/Product/label_checkbox.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_checkbox</name>
+   <tag></tag>
+   <elementGuidId>1802b62d-abdb-4cdb-be74-a8ad5a0c88d2</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//label[normalize-space()='${text}']//parent::dt//following-sibling::dd[1]//ul/li/label[contains(text(),'${value}')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
