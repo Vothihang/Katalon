4eeb481e27c9632527a6fa69553b0d0c20820c36
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:09:09 +0700

    Create a_Login.rs

---
+++ TestNopCommerce/Object Repository/Login/a_Login.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Login</name>
+   <tag></tag>
+   <elementGuidId>29f7bedf-1650-443d-b956-0ad414b94461</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(),'Log in')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
