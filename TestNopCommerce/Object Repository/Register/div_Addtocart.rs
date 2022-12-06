8473b2de8181d7c80d2164972ae3a96cbfcbc897
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 16:02:18 +0700

    Create div_Addtocart.rs

---
+++ TestNopCommerce/Object Repository/Register/div_Addtocart.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Addtocart</name>
+   <tag></tag>
+   <elementGuidId>48274e8c-ac19-45f2-bd4d-fdc8e290c85e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//div[@class=&quot;item-grid&quot;] //button[@class=&quot;button-2 product-box-add-to-cart-button&quot;])[${index}]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
