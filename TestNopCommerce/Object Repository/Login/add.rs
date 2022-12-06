4d597e19d54c41a1d5b24bd637c23d1ad653d1a5
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:10:12 +0700

    Create add.rs

---
+++ TestNopCommerce/Object Repository/Login/add.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>add</name>
+   <tag></tag>
+   <elementGuidId>e8d8bd3a-e8dc-4b21-8625-2adcb1e717fd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//div[@class=&quot;product-item&quot;] //button[@class=&quot;button-2 product-box-add-to-cart-button&quot;])[${rnd}]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
