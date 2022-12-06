69009f185c67ac669acb406fa37b44da593e6f94
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:23:24 +0700

    Create div_TextInput.rs

---
+++ TestNopCommerce/Object Repository/Product/Computers/div_TextInput.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_TextInput</name>
+   <tag></tag>
+   <elementGuidId>1659e4f1-53e8-4e8c-8868-3b7737449578</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='attributes']/dl/dd/input[@id = '${value}']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
