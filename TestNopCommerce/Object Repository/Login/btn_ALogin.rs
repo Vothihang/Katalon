64e212010d15235b6ec55eb044b7561ae1d9624d
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:10:52 +0700

    Create btn_ALogin.rs

---
+++ TestNopCommerce/Object Repository/Login/btn_ALogin.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_ALogin</name>
+   <tag></tag>
+   <elementGuidId>712cb7f0-4c01-486e-9e41-7a4727c2b77e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@class=&quot;${class}&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
