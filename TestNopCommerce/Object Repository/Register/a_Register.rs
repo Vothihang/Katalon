df30d567107804e8e97aefde4148fafa3ae9c157
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 16:00:02 +0700

    Create a_Register.rs

---
+++ TestNopCommerce/Object Repository/Register/a_Register.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Register</name>
+   <tag></tag>
+   <elementGuidId>943f97ad-d149-4861-9b9a-b759367daa5b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@class=&quot;${class}&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
