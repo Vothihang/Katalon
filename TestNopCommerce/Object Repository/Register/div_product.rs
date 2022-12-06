6ff0219b1b5a24b80468bd8277b5c86dea4d97ad
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 16:03:39 +0700

    Create div_product.rs

---
+++ TestNopCommerce/Object Repository/Register/div_product.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_product</name>
+   <tag></tag>
+   <elementGuidId>6787b4ba-ddab-4e25-a73f-5f879257be50</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;header-menu&quot;]//ul[@class=&quot;top-menu notmobile&quot;]/li/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
