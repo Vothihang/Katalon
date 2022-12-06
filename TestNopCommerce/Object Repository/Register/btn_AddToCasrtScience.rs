a6082530ab68bdf3a7a99174cbde2b0b2064d8ba
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 16:00:44 +0700

    Create btn_AddToCasrtScience.rs

---
+++ TestNopCommerce/Object Repository/Register/btn_AddToCasrtScience.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_AddToCasrtScience</name>
+   <tag></tag>
+   <elementGuidId>9df30e08-9d71-4b7a-bc4a-5e5d3fbbe0dd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@class=&quot;button-1 add-to-cart-button&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
