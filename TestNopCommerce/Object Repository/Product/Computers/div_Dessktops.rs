5e682fd213204d6eeb4b2aa3cb164a1f4fec73a8
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:18:25 +0700

    Create div_Dessktops.rs

---
+++ TestNopCommerce/Object Repository/Product/Computers/div_Dessktops.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Dessktops</name>
+   <tag></tag>
+   <elementGuidId>69012c1f-a84f-4edc-b30f-05b248b2cb09</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;item-grid&quot;]//h2//a[contains(text(),'${Text}')]    </value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
