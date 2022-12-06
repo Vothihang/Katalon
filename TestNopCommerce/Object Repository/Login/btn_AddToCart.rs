6e0aa197aacd65d169f6533254511e75d92969ac
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:10:32 +0700

    Create btn_AddToCart.rs

---
+++ TestNopCommerce/Object Repository/Login/btn_AddToCart.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_AddToCart</name>
+   <tag></tag>
+   <elementGuidId>2caed77b-fb59-4d0a-804d-65cac305e413</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//div[@class=&quot;product-item&quot;] //button[@class=&quot;button-2 product-box-add-to-cart-button&quot;])[${rnd}]
+
+</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
