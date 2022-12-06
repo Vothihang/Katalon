4ada582ad4e81c3f8d35485e45b979298c8458ec
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:19:03 +0700

    Create div_ListClothing.rs

---
+++ TestNopCommerce/Object Repository/Product/Computers/div_ListClothing.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_ListClothing</name>
+   <tag></tag>
+   <elementGuidId>7152124e-0c4d-494d-a044-fa177eaf3ff5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;item-grid&quot;]//div//div[3]//div[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
