544865cfd6594e0d7e7c64e09319ddeb4e879cc3
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:24:55 +0700

    Create li_Raido.rs

---
+++ TestNopCommerce/Object Repository/Product/Computers/li_Raido.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>li_Raido</name>
+   <tag></tag>
+   <elementGuidId>b091013f-2b08-4824-8ceb-161f88807b5a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;attributes&quot;]//dd/ul[@data-attr=&quot;${value}&quot;]/li/label</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
