a3986ee4c525e2996e57cc7f17f287af8d6f7490
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:13:43 +0700

    Create ul_table.rs

---
+++ TestNopCommerce/Object Repository/Login/ul_table.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ul_table</name>
+   <tag></tag>
+   <elementGuidId>8ecc4012-d577-435d-8f98-513b9b24e708</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//ul[@class=&quot;sublist first-level&quot;])[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
