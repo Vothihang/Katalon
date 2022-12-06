3d653d6ed2a40d88ee68465bf75ccacbf11a0687
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:46:24 +0700

    Create label_OptionDropdown.rs

---
+++ TestNopCommerce/Object Repository/Product/label_OptionDropdown.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_OptionDropdown</name>
+   <tag></tag>
+   <elementGuidId>020888e7-779a-4934-8e34-c34d2eb8ba0b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//label[normalize-space()='${text}']//parent::dt//following-sibling::dd[1]//select/option[contains(text(),'${value}')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
