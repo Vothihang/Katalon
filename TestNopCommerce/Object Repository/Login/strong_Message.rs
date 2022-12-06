4403c0fca9f6f8367845c1fa7e11b76b6d5ec210
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:12:52 +0700

    Create strong_Message.rs

---
+++ TestNopCommerce/Object Repository/Login/strong_Message.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>strong_Message</name>
+   <tag></tag>
+   <elementGuidId>2f9d0721-48a1-43ce-8501-6abfbb870e5a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//strong[contains(text(),'Your order has been successfully processed!')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
