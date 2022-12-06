4bdc3b7f22370e9871d986fa21bbce42ab9a86b3
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:47:53 +0700

    Create p_message.rs

---
+++ TestNopCommerce/Object Repository/Product/p_message.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>p_message</name>
+   <tag></tag>
+   <elementGuidId>24d52a54-fc43-4bc2-8ea2-8c4fd98b429a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//p[@class=&quot;content&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
