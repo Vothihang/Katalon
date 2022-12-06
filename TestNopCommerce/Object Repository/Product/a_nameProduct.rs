c93df15ded8c0559a291bb25157643bd577246fc
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:14:44 +0700

    Create a_nameProduct.rs

---
+++ TestNopCommerce/Object Repository/Product/a_nameProduct.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_nameProduct</name>
+   <tag></tag>
+   <elementGuidId>6cc4ded5-3c9b-4a75-9347-86b793522cf4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;product-grid&quot;]//h2[@class=&quot;product-title&quot;]//a[contains(text(),'${value}')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
