c26c27a8905d270a524c6e64225910ef85dfd59f
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:24:14 +0700

    Create div_Total.rs

---
+++ TestNopCommerce/Object Repository/Product/Computers/div_Total.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Total</name>
+   <tag></tag>
+   <elementGuidId>bb340210-e14b-4fb0-ada8-32baeaacc28e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value> //td[@class=&quot;unit-price&quot;]//span </value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
