148529a87f115ba5387b704d34a01b170dd821fd
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:26:06 +0700

    Create select_Ram.rs

---
+++ TestNopCommerce/Object Repository/Product/Computers/select_Ram.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_Ram</name>
+   <tag></tag>
+   <elementGuidId>cfb1ac66-8af1-48f3-a945-5dac8c2a9042</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@name=&quot;product_attribute_2&quot;]//option</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
