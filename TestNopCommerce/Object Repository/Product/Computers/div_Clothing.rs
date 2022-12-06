11558329f3fcf39ba6c69cc2f4c9f837b75b8685
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:17:29 +0700

    Create div_Clothing.rs

---
+++ TestNopCommerce/Object Repository/Product/Computers/div_Clothing.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Clothing</name>
+   <tag></tag>
+   <elementGuidId>ac4de5cf-4eaf-44f1-9a3e-3eb31e890cb6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;item-grid&quot;]//h2//a[contains(text(),&quot;${Text}&quot;)]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
