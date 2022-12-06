724e688e58af0e781a356946d4f9af7dc3300fc0
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:25:20 +0700

    Create select_Processor.rs

---
+++ TestNopCommerce/Object Repository/Product/Computers/select_Processor.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_Processor</name>
+   <tag></tag>
+   <elementGuidId>ec8c373b-f91b-40cf-a706-70ab0fa9b0f6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@name=&quot;product_attribute_1&quot;]/option[contains(text(),'${value}')] </value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
