82a392b2d515352f7cea6db1cadb87c919dcf0c9
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:16:23 +0700

    Create btn_Notebooks.rs

---
+++ TestNopCommerce/Object Repository/Product/btn_Notebooks.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Notebooks</name>
+   <tag></tag>
+   <elementGuidId>0c28ec78-178b-416f-abe4-3e183866db54</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;product-grid&quot;]//h2[@class=&quot;product-title&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
