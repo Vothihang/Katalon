cf71fab4f37b2273fe819528eebaa05f87f5fc83
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:46:48 +0700

    Create label_radio.rs

---
+++ TestNopCommerce/Object Repository/Product/label_radio.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_radio</name>
+   <tag></tag>
+   <elementGuidId>ecce273d-2887-43ed-9362-1f0244c12fa5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//label[normalize-space()='${text}']//parent::dt//following-sibling::dd[1]//ul/li/label[contains(text(),'${value}')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
