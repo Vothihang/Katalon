4483ece0ff4eae59baf7b4734db6a7fa8a404abf
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:28:37 +0700

    Create label_input.rs

---
+++ TestNopCommerce/Object Repository/Product/label_input.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_input</name>
+   <tag></tag>
+   <elementGuidId>69e33e69-431e-49d8-af69-d89011c659ae</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//label[normalize-space()=&quot;${text}&quot;]//parent::dt//following-sibling::dd[1]//input</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
