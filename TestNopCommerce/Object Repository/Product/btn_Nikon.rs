b24a9fb775f0f2ad0307e10a3fcfaa946e0f7740
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:16:01 +0700

    Create btn_Nikon.rs

---
+++ TestNopCommerce/Object Repository/Product/btn_Nikon.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Nikon</name>
+   <tag></tag>
+   <elementGuidId>5830a97a-1e44-45af-ba27-6dae64c789c4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//button[@class=&quot;button-1 add-to-cart-button&quot;])[${value}]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
