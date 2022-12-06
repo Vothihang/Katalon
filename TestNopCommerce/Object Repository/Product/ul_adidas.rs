e043ec14593408fc720f2150efab55bbe2f539aa
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:51:51 +0700

    Create ul_adidas.rs

---
+++ TestNopCommerce/Object Repository/Product/ul_adidas.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ul_adidas</name>
+   <tag></tag>
+   <elementGuidId>39c4994e-ad07-4e92-a94f-ad5f1d3464b5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//ul[@class=&quot;option-list attribute-squares color-squares&quot;]//li[${value}]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
