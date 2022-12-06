b5f50e87f538b56c2bedb34888d148a64a775e35
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:12:09 +0700

    Create label_table.rs

---
+++ TestNopCommerce/Object Repository/Login/label_table.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_table</name>
+   <tag></tag>
+   <elementGuidId>6a4c0295-be8d-4d0d-a1a9-c6fd482d2308</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//label[@for=&quot;billing-address-select&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
