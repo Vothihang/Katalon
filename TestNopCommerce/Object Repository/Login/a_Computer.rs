4821fdfff03686ea1740380e93b7ef2a2a7a7847
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:08:50 +0700

    Create a_Computer.rs

---
+++ TestNopCommerce/Object Repository/Login/a_Computer.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Computer</name>
+   <tag></tag>
+   <elementGuidId>7468c9f4-2c0b-498e-b794-4dfd0abeb217</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//a[contains(text(),'${text}')])[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
