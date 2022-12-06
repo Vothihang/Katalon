dcd90226d3cb72e956901dae285f49590b4e641e
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:09:28 +0700

    Create a_Navigate.rs

---
+++ TestNopCommerce/Object Repository/Login/a_Navigate.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Navigate</name>
+   <tag></tag>
+   <elementGuidId>52b7fe2f-1a9a-4379-9f45-b9b78b24721f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//a[contains(.,'${Text}')])[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
