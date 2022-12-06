1eb3892741d2d0cda3ea83a1d688a1e37c921a59
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:49:43 +0700

    Create select_radio.rs

---
+++ TestNopCommerce/Object Repository/Product/select_radio.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_radio</name>
+   <tag></tag>
+   <elementGuidId>34632b23-206d-494e-b2a9-d0293faa342d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//ul[@data-attr=&quot;3&quot;]/li/label</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
