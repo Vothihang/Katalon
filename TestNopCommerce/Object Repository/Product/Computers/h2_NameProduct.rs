fee5f3f5dd68c17202e84d797acb68d8a25f1830
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:24:35 +0700

    Create h2_NameProduct.rs

---
+++ TestNopCommerce/Object Repository/Product/Computers/h2_NameProduct.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h2_NameProduct</name>
+   <tag></tag>
+   <elementGuidId>008fd74f-1b17-463c-a43c-3f07120827d1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//td[@class=&quot;unit-price&quot;]/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
