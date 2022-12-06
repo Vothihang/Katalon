532fc30a470e0e9bbb8ba9211f3a6eec005c05c3
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 16:03:04 +0700

    Create div_message.rs

---
+++ TestNopCommerce/Object Repository/Register/div_message.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_message</name>
+   <tag></tag>
+   <elementGuidId>242b27ab-833d-4c23-8221-3f0f55b6392b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//p[contains(text(),&quot;The product has been added to your&quot;)]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
