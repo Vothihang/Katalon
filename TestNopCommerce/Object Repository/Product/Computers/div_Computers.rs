017f5125e620f272068db4848afc5d7ff51f7b26
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:17:49 +0700

    Create div_Computers.rs

---
+++ TestNopCommerce/Object Repository/Product/Computers/div_Computers.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Computers</name>
+   <tag></tag>
+   <elementGuidId>2e5113a5-a8b6-49ae-9d29-e6fff5a33c0d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;item-grid&quot;]//h2/a</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
