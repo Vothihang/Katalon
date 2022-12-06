5ce41fc0be5ea62341f9a1f43b7efe334e05d0fa
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:22:59 +0700

    Create div_Processor.rs

---
+++ TestNopCommerce/Object Repository/Product/Computers/div_Processor.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Processor</name>
+   <tag></tag>
+   <elementGuidId>25b5ccaa-8348-47aa-a7eb-7a82b69a92c5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@name=&quot;product_attribute_1&quot;]//option</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
