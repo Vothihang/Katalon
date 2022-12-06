9921e3a8b6e6457476ce16cb3b6df3517d45fd1e
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-06 15:25:45 +0700

    Create select_ProductAttribute.rs

---
+++ TestNopCommerce/Object Repository/Product/Computers/select_ProductAttribute.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_ProductAttribute</name>
+   <tag></tag>
+   <elementGuidId>f1ec0b34-154f-4b19-96c0-3314fa479caa</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class = 'overview']//select[@data-attr ='${value}']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
