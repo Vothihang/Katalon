15a167e56aa6c89d1e0e750e0cf93758a8a35c75
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:21:35 +0700

    Create btn_Rent.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/btn_Rent.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Rent</name>
+   <tag></tag>
+   <elementGuidId>94f32f2e-a587-47e3-a36d-4d999cdd5a39</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[contains(.,'Rent')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
