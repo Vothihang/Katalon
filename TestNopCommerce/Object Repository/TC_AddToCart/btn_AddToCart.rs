20517bb6b54598cf7f0746dae713055c8d904b93
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:21:28 +0700

    Create btn_AddToCart.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/btn_AddToCart.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_AddToCart</name>
+   <tag></tag>
+   <elementGuidId>1360a268-41b9-454a-9a60-a02c7d67840d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//button[contains(.,'Add to cart')])[${index}]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
