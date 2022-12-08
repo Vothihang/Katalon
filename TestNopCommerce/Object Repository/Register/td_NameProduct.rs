a4d85b2a5272719b328d6cc287e598738ed788f6
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:20:49 +0700

    Create td_NameProduct.rs

---
+++ TestNopCommerce/Object Repository/Register/td_NameProduct.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>td_NameProduct</name>
+   <tag></tag>
+   <elementGuidId>cb3df3f7-4ce4-4af9-9d34-1c92e2e35592</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;product-name&quot;]/h1</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
