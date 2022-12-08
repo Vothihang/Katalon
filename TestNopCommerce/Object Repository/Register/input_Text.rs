689895c9bdd619e4cec5054739d3d9f5c27890ce
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:20:27 +0700

    Create input_Text.rs

---
+++ TestNopCommerce/Object Repository/Register/input_Text.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_Text</name>
+   <tag></tag>
+   <elementGuidId>8ac5525e-9e08-49ce-a4a1-d499a0384ac0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name=&quot;${text}&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
