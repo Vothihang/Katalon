2fb4e30e5c701001fb98e0849f75b98acbbe3a62
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:20:33 +0700

    Create li_Error.rs

---
+++ TestNopCommerce/Object Repository/Register/li_Error.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>li_Error</name>
+   <tag></tag>
+   <elementGuidId>4c2e7c27-d88f-4085-942a-0091adc0e292</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//li[contains(.,&quot;The specified email already exists&quot;)]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
