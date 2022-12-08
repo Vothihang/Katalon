97adb6ec96ff7f5120e3fa281099fa462260cc45
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:21:06 +0700

    Create a_Accessories.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/a_Accessories.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Accessories</name>
+   <tag></tag>
+   <elementGuidId>c6255470-e816-4a25-831a-586709b59973</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(.,' Accessories ')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
