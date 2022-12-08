aee9826a85ec88b48e6fbf0b1d69ec23beb4ccbc
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:22:07 +0700

    Create select_Computer.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/select_Computer.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_Computer</name>
+   <tag></tag>
+   <elementGuidId>722c1441-b8fa-4872-9cd4-dbe5535fafe1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@name=&quot;${name}&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
