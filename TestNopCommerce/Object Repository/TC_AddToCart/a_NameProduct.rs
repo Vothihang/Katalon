e400689b99e4bfc699fe7225a02411aada5a6f71
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:21:24 +0700

    Create a_NameProduct.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/a_NameProduct.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_NameProduct</name>
+   <tag></tag>
+   <elementGuidId>09e1279b-18bd-4d4f-849e-a5176d5ff198</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(.,'HTC One M8 Android L 5.0 Lollipop')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
