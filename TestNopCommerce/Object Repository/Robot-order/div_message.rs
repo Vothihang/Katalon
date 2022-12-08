fc139787de7515f919e810b25b99deef57079c88
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:46:48 +0700

    Create div_message.rs

---
+++ TestNopCommerce/Object Repository/Robot-order/div_message.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_message</name>
+   <tag></tag>
+   <elementGuidId>272f45cf-9355-4442-8e9d-2d0707f3dea2</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;alert alert-danger&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
