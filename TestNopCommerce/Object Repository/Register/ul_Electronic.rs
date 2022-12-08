af0afd421286b27e5b8094e5f596a8d739b7ca24
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:20:54 +0700

    Create ul_Electronic.rs

---
+++ TestNopCommerce/Object Repository/Register/ul_Electronic.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ul_Electronic</name>
+   <tag></tag>
+   <elementGuidId>11502462-b648-4734-b067-21cf75d2687c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//ul[@class=&quot;top-menu notmobile&quot;]//li[2]//ul//li/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
