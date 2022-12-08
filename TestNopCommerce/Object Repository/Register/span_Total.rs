d0006931e7e4c9a5a30a6da054f45d57e7ef4d84
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:20:45 +0700

    Create span_Total.rs

---
+++ TestNopCommerce/Object Repository/Register/span_Total.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Total</name>
+   <tag></tag>
+   <elementGuidId>2e4e5b91-9e91-46c2-ab89-82c44d6d1438</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;product-price&quot;]/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
