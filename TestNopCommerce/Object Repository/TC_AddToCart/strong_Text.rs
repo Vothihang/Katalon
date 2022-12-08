b0fb9aa391fe6801aa082c8e26fa8ce24c1378ab
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:22:11 +0700

    Create strong_Text.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/strong_Text.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>strong_Text</name>
+   <tag></tag>
+   <elementGuidId>59578aa0-dd27-4988-bb7e-0ac27772d377</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//strong[contains(.,'Featured products')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
