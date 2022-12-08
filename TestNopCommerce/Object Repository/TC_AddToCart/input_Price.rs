a7939dba7177b9f53b910acc93690e8349e35ba7
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:21:59 +0700

    Create input_Price.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/input_Price.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_Price</name>
+   <tag></tag>
+   <elementGuidId>d0aab904-b3e1-4a6f-bdf7-3012b0b807b9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id=&quot;addtocart_35_CustomerEnteredPrice&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
