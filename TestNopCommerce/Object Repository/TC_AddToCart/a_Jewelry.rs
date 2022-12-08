8ebbea1f142dab2cb25a1497d3b7dbfe1ed7aa9f
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:21:19 +0700

    Create a_Jewelry.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/a_Jewelry.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Jewelry</name>
+   <tag></tag>
+   <elementGuidId>9eb3440e-4d3d-467d-84c8-c1584b98201e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//a[contains(., 'Jewelry ')])[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
