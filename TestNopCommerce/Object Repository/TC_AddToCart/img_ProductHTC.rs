b25f4183532411037201c45fda2f940700e652b5
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:21:51 +0700

    Create img_ProductHTC.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/img_ProductHTC.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>img_ProductHTC</name>
+   <tag></tag>
+   <elementGuidId>e760c1e3-c2c7-499e-a39f-1ddafea43789</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//img[@alt=&quot;Picture of HTC One M8 Android L 5.0 Lollipop&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
