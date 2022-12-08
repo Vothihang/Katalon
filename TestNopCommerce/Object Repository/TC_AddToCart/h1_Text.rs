faa3b3520fc381c57f45d10d0f7f79f521160225
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:21:48 +0700

    Create h1_Text.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/h1_Text.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h1_Text</name>
+   <tag></tag>
+   <elementGuidId>4e593b56-0ac4-4c38-ba7a-b8a7e5136e1d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//h1[contains(.,'HTC One M8 Android L 5.0 Lollipop')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
