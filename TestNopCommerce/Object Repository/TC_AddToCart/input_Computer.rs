5d5dc69d55ed7841530a1ecea3078833be504ebf
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:21:55 +0700

    Create input_Computer.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/input_Computer.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_Computer</name>
+   <tag></tag>
+   <elementGuidId>2d63758d-1910-4be2-a70b-287929291a30</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id=&quot;${input}&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
