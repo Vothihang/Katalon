47629b0240b26018b1a2eb2751133751566fbb3c
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:46:43 +0700

    Create div_body.rs

---
+++ TestNopCommerce/Object Repository/Robot-order/div_body.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_body</name>
+   <tag></tag>
+   <elementGuidId>743a38bb-272f-4cb0-a121-ec5ebb8faa85</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//div[@class=&quot;stacked&quot;]//input[@class=&quot;form-check-input&quot;])[${value}]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
