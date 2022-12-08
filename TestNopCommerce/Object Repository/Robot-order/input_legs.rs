9044ea40bd659c46ac2a3fa0276b82be76bfd6f8
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:46:58 +0700

    Create input_legs.rs

---
+++ TestNopCommerce/Object Repository/Robot-order/input_legs.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_legs</name>
+   <tag></tag>
+   <elementGuidId>ce4371a3-dea4-4c59-867a-a4224100e2d3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//input[@class=&quot;form-control&quot;])[${value}]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
