afc7538ff6fb1d9eef93ca899281cab51899cfcb
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:46:31 +0700

    Create btn_order-another.rs

---
+++ TestNopCommerce/Object Repository/Robot-order/btn_order-another.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_order-another</name>
+   <tag></tag>
+   <elementGuidId>da2d286e-2fc7-493b-9b37-0b162fe328a4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id=&quot;order-another&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
