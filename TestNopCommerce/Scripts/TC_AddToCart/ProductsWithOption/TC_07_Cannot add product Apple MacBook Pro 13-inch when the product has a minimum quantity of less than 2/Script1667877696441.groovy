401eae6b497e91a73cf147bf0e445434b17987f8
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:23:44 +0700

    Create Script1667877696441.groovy

---
+++ TestNopCommerce/Scripts/TC_AddToCart/ProductsWithOption/TC_07_Cannot add product Apple MacBook Pro 13-inch when the product has a minimum quantity of less than 2/Script1667877696441.groovy
@@ -0,0 +1,49 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+'openBrowser NopCommerce'
+WebUI.openBrowser('https://demo.nopcommerce.com/')
+
+'maximaze NopCommerce '
+WebUI.maximizeWindow()
+
+'scroll to Featured products'
+WebUI.scrollToElement(findTestObject('Object Repository/TC_AddToCart/strong_Text'), 2)
+
+'click add to cart'
+WebUI.click(findTestObject('Object Repository/TC_AddToCart/btn_AddToCart',[('index'):'2']))
+
+'input quantity '
+WebUI.setText(findTestObject('Object Repository/TC_AddToCart/input_QuantityValue',[('quantity'):'addtocart_4.EnteredQuantity']), '1')
+
+'click add to cart'
+WebUI.click(findTestObject('Object Repository/TC_AddToCart/btn_AddToCart',[('index'):'1']))
+
+'verify message add product to cart unsuccessfully'
+WebUI.verifyTextPresent('The minimum quantity allowed for purchase is 2.', false)
+'take screenshot'
+Date today = new Date()
+
+   String dateFolder = today.format('dd.MM.yyyy').replace('.', '-')
+
+   String nowTime = today.format('dd.MM.yyyy hh:mm:ss').replace(':', '.')
+   
+WebUI.takeScreenshot((((((('ScreenShots'  + '/Error/') + dateFolder) + '/') + 'SS1') + '_') +   nowTime) + '.png')
+'closeBrowser'
+WebUI.closeBrowser()
