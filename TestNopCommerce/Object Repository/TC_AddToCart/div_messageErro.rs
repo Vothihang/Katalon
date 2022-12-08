1bdd427cc9b5e24bf7fcabc8dca48b857d09f456
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:21:44 +0700

    Create div_messageErro.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/div_messageErro.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_messageErro</name>
+   <tag></tag>
+   <elementGuidId>de9ffe2a-a216-4731-889c-718ff6e17734</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;bar-notification error&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
