94824e96261235b6ed9d2c8cff0a238996a18ffd
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:46:24 +0700

    Create btn_ok.rs

---
+++ TestNopCommerce/Object Repository/Robot-order/btn_ok.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_ok</name>
+   <tag></tag>
+   <elementGuidId>ba94b134-c615-43ac-9406-e29cc7b8cfaa</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@class=&quot;btn btn-dark&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
