dfdd8f3b50b2503026a049f6aa8db6f552b195e8
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:21:11 +0700

    Create a_Apparel.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/a_Apparel.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Apparel</name>
+   <tag></tag>
+   <elementGuidId>fb3ca18f-0c60-4bca-aad2-5edf84e1463b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//a[contains(., 'Apparel ')])[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
