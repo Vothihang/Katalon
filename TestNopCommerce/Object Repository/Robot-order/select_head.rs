192ea10184f112cb58d5903e8a35ee2698745a75
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:47:02 +0700

    Create select_head.rs

---
+++ TestNopCommerce/Object Repository/Robot-order/select_head.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_head</name>
+   <tag></tag>
+   <elementGuidId>182ea761-451d-4cab-93f2-5b17cc2d4a08</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@id=&quot;head&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
