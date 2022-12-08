f515282c2c79621c6cb1f5f8cc5ab22c1cb6b524
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:21:15 +0700

    Create a_DigitalDowload.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/a_DigitalDowload.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_DigitalDowload</name>
+   <tag></tag>
+   <elementGuidId>2e984cf2-691d-4945-8fe3-93e86fc7771a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//a[contains(.,'Digital downloads ')])[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
