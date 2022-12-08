24a36dede3d777490340c323fcad9c823bcf6024
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:22:02 +0700

    Create input_QuantityValue.rs

---
+++ TestNopCommerce/Object Repository/TC_AddToCart/input_QuantityValue.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_QuantityValue</name>
+   <tag></tag>
+   <elementGuidId>603d15e2-b0c2-4275-82b6-25aec41025bd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name=&quot;${quantity}&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
