dd78fe59a6375cb891d5f2b185c5ed59ec1290c1
Vothihang <105780723+Vothihang@users.noreply.github.com>
2022-12-08 08:21:00 +0700

    Create ul_Product.rs

---
+++ TestNopCommerce/Object Repository/Register/ul_Product.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ul_Product</name>
+   <tag></tag>
+   <elementGuidId>f82c735f-e18c-42c5-a770-bbcff5be4f3e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//ul[@class=&quot;top-menu notmobile&quot;]//li[1]//ul//li/a
+</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
